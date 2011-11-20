569ff9860e5f337a6ae6233a1a7eb0a889ce08e9
David Danford <ddanford@gmail.com>
2011-11-20 12:17:01 -0700

    Class file for Score.

---
+++ Score.py
@@ -0,0 +1,13 @@
+class Score:
+	def __init__(self, parts):
+		self.parts = parts
+
+	def printScore(self):
+		output = '<score-partwise>\n'
+		output += '\t<part-list>\n'
+		for part in self.parts:
+			output += part.printPartList()
+		output += '\t</part-list>\n'
+		for part in self.parts:
+			output += part.printPart()
+		output += '</score-partwise>\n'
